8c4fbe352e2a1614ecb4941582f03ea6428e8a8c
ian <me@iany.me>
2020-03-28 06:56:20 +0800

    Refactor lua compiler

--- animagus-lua/README.md
+++ animagus-lua/README.md
@@ -73,3 +73,49 @@ lua5.1 animagus-lua/compiler.lua animagus-lua/examples/balance.lua | jq .
   ]
 }
 ```
+
+## MetaLua AST
+
+```
+block: { stat* }
+
+stat:
+  `Do{ stat* }
+| `Set{ {lhs+} {expr+} }                    -- lhs1, lhs2... = e1, e2...
+| `While{ expr block }                      -- while e do b end
+| `Repeat{ block expr }                     -- repeat b until e
+| `If{ (expr block)+ block? }               -- if e1 then b1 [elseif e2 then b2] ... [else bn] end
+| `Fornum{ ident expr expr expr? block }    -- for ident = e, e[, e] do b end
+| `Forin{ {ident+} {expr+} block }          -- for i1, i2... in e1, e2... do b end
+| `Local{ {ident+} {expr+}? }               -- local i1, i2... = e1, e2...
+| `Localrec{ ident expr }                   -- only used for 'local function'
+| `Goto{ <string> }                         -- goto str
+| `Label{ <string> }                        -- ::str::
+| `Return{ <expr*> }                        -- return e1, e2...
+| `Break                                    -- break
+| apply
+
+expr:
+  `Nil  |  `Dots  |  `True  |  `False
+| `Number{ <number> }
+| `String{ <string> }
+| `Function{ { ident* `Dots? } block }
+| `Table{ ( `Pair{ expr expr } | expr )* }
+| `Op{ opid expr expr? }
+| `Paren{ expr }       -- significant to cut multiple values returns
+| apply
+| lhs
+
+apply:
+  `Call{ expr expr* }
+| `Invoke{ expr `String{ <string> } expr* }
+
+ident: `Id{ <string> }
+
+lhs: ident | `Index{ expr expr }
+
+opid: 'add'   | 'sub'   | 'mul'   | 'div'
+    | 'mod'   | 'pow'   | 'concat'| 'eq'
+    | 'lt'    | 'le'    | 'and'   | 'or'
+    | 'not'   | 'len'
+```
--- animagus-lua/compiler.lua
+++ animagus-lua/compiler.lua
@@ -14,7 +14,7 @@ STAT_TAGS = {
   Break = true
 }
 
-local function ast_tostring(ast,a)
+local function ast_tostring(ast, a)
 	a = a or 0
 	local white = ("  "):rep(a+1)
 	local str = tostring(ast)
@@ -24,47 +24,51 @@ local function ast_tostring(ast,a)
 
 	local res
 	if ast.tag then
-	  res = {"`" .. ast.tag .. " {"}
+	  res = { "`" .. ast.tag .. " {" }
   else
-	  res = {"{"}
+	  res = { "{" }
   end
 	for k,v in ipairs(ast) do
-		table.insert(res,white..ast_tostring(v, a + 1))
+		table.insert(res,white .. ast_tostring(v, a + 1))
 	end
 	white = white:sub(3)
-	table.insert(res,white.."}")
-	return table.concat(res,"\n")
+	table.insert(res,white .. "}")
+	return table.concat(res, "\n")
 end
 
-local mlc = require 'metalua.compiler'.new()
-local ast = mlc:srcfile_to_ast(arg[1])
-
-local root_scope = { ["return"] = {}, vars = {}, compiled = {}}
 local Scope = {}
 
-setmetatable(root_scope, { __index = Scope })
+function Scope.new(parent_scope)
+  local scope = { vars = {}, parent = parent_scope, ret = nil }
+  setmetatable(scope, { __index = Scope })
+  if parent_scope ~= nil then
+    setmetatable(scope.vars, { __index = parent_scope.vars })
+  end
+  return scope
+end
 
 function Scope:push()
-  local new_scope = { vars = {}, compiled = {}, parent = self }
-  setmetatable(new_scope, { __index = Scope })
-  setmetatable(new_scope.vars, { __index = self.vars })
-  return new_scope
+  return Scope.new(self)
 end
 
 function Scope:eval_stat(stat)
   if stat.tag == "Local" then
     -- | `Local{ {ident+} {expr+}? }               -- local i1, i2... = e1, e2...
-    for i, ident in ipairs(stat[1]) do
+    local idents, exprs = unpack(stat)
+    for i, ident in ipairs(idents) do
       assert(ident.tag == "Id")
-      self.vars[ident[1]] = { scope = self, expr = stat[2][i] }
+      self.vars[ident[1]] = { scope = self, expr = exprs[i] }
     end
   elseif stat.tag == "Localrec" then
     -- | `Localrec{ ident expr }                   -- only used for 'local function'
-    assert(stat[1][1].tag == "Id", "Invalid Localrec ident " .. ast_tostring(stat[1]))
-    self.vars[stat[1][1][1]] = { scope = self, expr = stat[2][1] }
+    local idents, exprs = unpack(stat)
+    local ident = idents[1]
+    local expr = exprs[1]
+    assert(ident.tag == "Id", "Invalid Localrec ident " .. ast_tostring(ident))
+    self.vars[ident[1]] = { scope = self, expr = expr }
   elseif stat.tag == "Return" then
     -- | `Return{ <expr*> }                        -- return e1, e2...
-    self["return"] = { scope = self, expr = stat }
+    self.ret = { scope = self, expr = stat }
   else
     assert(false, "unknown eval_stat " .. tostring(stat.tag) .. ": " .. ast_tostring(stat))
   end
@@ -86,7 +90,7 @@ function Scope:compile_call(expr)
     for _, stat in ipairs(expr[2]) do
       scope:eval_stat(stat)
     end
-    return scope:compile_expr(scope["return"].expr[1])
+    return scope:compile_expr(scope.ret.expr[1])
   end
   assert(false, "unknown compile_call " .. tostring(expr.tag) .. ": " .. ast_tostring(expr))
 end
@@ -156,73 +160,40 @@ function Scope:compile_expr(expr)
   assert(false, "unknown compile_expr " .. tostring(expr.tag) .. ": " .. ast_tostring(expr))
 end
 
-for _, stat in ipairs(ast) do
-  root_scope:eval_stat(stat)
-end
+function main(input_path, output_path, format)
+  local mlc = require 'metalua.compiler'.new()
+  local ast = mlc:srcfile_to_ast(input_path)
 
-local root_ast = root_scope["return"].expr[1]
-assert(root_ast and root_ast.tag == "Table")
+  local root_scope = Scope.new()
 
-animagus_ast = {
-  calls = {},
-  streams = {}
-}
+  for _, stat in ipairs(ast) do
+    root_scope:eval_stat(stat)
+  end
 
-for _, kv in ipairs(root_ast) do
-  assert(kv.tag == "Pair")
-  if kv[1][1] == "calls" then
-    for _, call_pair in ipairs(kv[2]) do
-      assert(call_pair.tag == "Pair")
-      k, v = unpack(call_pair)
-      assert(k.tag == "String")
-      table.insert(animagus_ast.calls, { name = k[1], result = root_scope:compile_call(v) })
+  local root_ast = root_scope.ret.expr[1]
+  assert(root_ast and root_ast.tag == "Table")
+
+  animagus_ast = {
+    calls = {},
+    streams = {}
+  }
+
+  for _, kv in ipairs(root_ast) do
+    assert(kv.tag == "Pair")
+    if kv[1][1] == "calls" then
+      for _, call_pair in ipairs(kv[2]) do
+        assert(call_pair.tag == "Pair")
+        k, v = unpack(call_pair)
+        assert(k.tag == "String")
+        table.insert(animagus_ast.calls, { name = k[1], result = root_scope:compile_call(v) })
+      end
     end
   end
+
+  local json = require "json"
+  print(json.encode(animagus_ast))
 end
 
-local json = require "json"
-print(json.encode(animagus_ast))
-
-
--- block: { stat* }
---
--- stat:
---   `Do{ stat* }
--- | `Set{ {lhs+} {expr+} }                    -- lhs1, lhs2... = e1, e2...
--- | `While{ expr block }                      -- while e do b end
--- | `Repeat{ block expr }                     -- repeat b until e
--- | `If{ (expr block)+ block? }               -- if e1 then b1 [elseif e2 then b2] ... [else bn] end
--- | `Fornum{ ident expr expr expr? block }    -- for ident = e, e[, e] do b end
--- | `Forin{ {ident+} {expr+} block }          -- for i1, i2... in e1, e2... do b end
--- | `Local{ {ident+} {expr+}? }               -- local i1, i2... = e1, e2...
--- | `Localrec{ ident expr }                   -- only used for 'local function'
--- | `Goto{ <string> }                         -- goto str
--- | `Label{ <string> }                        -- ::str::
--- | `Return{ <expr*> }                        -- return e1, e2...
--- | `Break                                    -- break
--- | apply
---
--- expr:
---   `Nil  |  `Dots  |  `True  |  `False
--- | `Number{ <number> }
--- | `String{ <string> }
--- | `Function{ { ident* `Dots? } block }
--- | `Table{ ( `Pair{ expr expr } | expr )* }
--- | `Op{ opid expr expr? }
--- | `Paren{ expr }       -- significant to cut multiple values returns
--- | apply
--- | lhs
---
--- apply:
---   `Call{ expr expr* }
--- | `Invoke{ expr `String{ <string> } expr* }
---
--- ident: `Id{ <string> }
---
--- lhs: ident | `Index{ expr expr }
---
--- opid: 'add'   | 'sub'   | 'mul'   | 'div'
---     | 'mod'   | 'pow'   | 'concat'| 'eq'
---     | 'lt'    | 'le'    | 'and'   | 'or'
---     | 'not'   | 'len'
---
+local format = "proto"
+local input_path
+main(arg[1], arg[2])
